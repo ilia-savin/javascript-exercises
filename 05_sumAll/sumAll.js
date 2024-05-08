d85203ad39fa40dbd04980eef81a03472d26f8d9
ilia-savin <ilia.savin@outlook.com>
2024-05-08 12:52:40 +0200

    05 js complete assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if (!Number.isInteger(number1) || !Number.isInteger(number2)) return "ERROR";
+    if (number1 < 0 || number2 < 0) return "ERROR";
+    if (number1 > number2) {
+      const temp = number1;
+      number1 = number2;
+      number2 = temp;
+    }
+    
+    let sum = 0;
+    for (let i = number1; i <= number2; number1++) {
+        sum += number1;
+    }
+    return sum;
 };
 
 // Do not edit below this line
