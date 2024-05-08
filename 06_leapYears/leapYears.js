ab0fdead11c80aa14fbc1711563259cf1b23e37c
ilia-savin <ilia.savin@outlook.com>
2024-05-08 13:56:04 +0200

    06 js complete assignment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
+
+// or like this: return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
 
 // Do not edit below this line
 module.exports = leapYears;
