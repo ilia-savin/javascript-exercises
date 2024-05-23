210fc38d0cb7cfde4b0a2d859ba4c4f7944e0d8d
ilia-savin <ilia.savin@outlook.com>
2024-05-23 13:33:53 +0200

    solved assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+	return array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  });
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+  });};
 
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return factorial(n-1) * n;
 };
 
 // Do not edit below this line
