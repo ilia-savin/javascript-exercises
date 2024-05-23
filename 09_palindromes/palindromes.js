73079c8f3e37dcdc3787b6b3d4e44291ba4d31ef
ilia-savin <ilia.savin@outlook.com>
2024-05-23 14:07:37 +0200

    solved assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanString = string.replace(/[^\w]|_/g, "").toLowerCase();
+    let reversedString = cleanString.split("").reverse().join("");
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
