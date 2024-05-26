ec1eae895663b43b45f9ec0891481ed89ac61444
ilia-savin <ilia.savin@outlook.com>
2024-05-26 22:16:00 +0200

    solution assignment 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let intNumber = Number(n);
+    if (intNumber < 0) {
+        return "OOPS"
+    } else if (intNumber === 0) {
+        return 0;
+    } else if (intNumber === 1) {
+        return 1;
+    } else {
+        return fibonacci(intNumber - 1) + fibonacci(intNumber - 2);
+    }
 };
 
 // Do not edit below this line
